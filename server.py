986617db70df9a88b2ec0c07bf8b39e721e6221d
valentin-th <valentintranhuu@gmail.com>
2022-11-16 14:41:09 +0000

    server tcp

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
